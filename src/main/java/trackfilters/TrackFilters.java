afb9895d89b5e96e0981a9e4cfce66db2cbdc059
NikolayNN <nikolay.horushko@gmail.com>
2022-02-18 11:16:40 +0300

    add bean in Spring application context

--- src/main/java/trackfilters/TrackFilters.java
+++ src/main/java/trackfilters/TrackFilters.java
@@ -1,10 +1,12 @@
 package trackfilters;
 
 import by.nhorushko.distancecalculator.LatLngAlt;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TrackFilters {
 
     /**
