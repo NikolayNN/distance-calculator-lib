35091ee71a8b5c822ad51b4ceba088fdb5b46658
NikolayNN <nikolay.horushko@gmail.com>
2022-04-24 11:24:15 +0300

    update docs

--- src/main/java/by/nhorushko/distancecalculator/LatLngAlt.java
+++ src/main/java/by/nhorushko/distancecalculator/LatLngAlt.java
@@ -3,10 +3,19 @@ package by.nhorushko.distancecalculator;
 import java.time.Instant;
 
 public interface LatLngAlt {
+
     Instant getDatetime();
+
     float getLatitude();
+
     float getLongitude();
+
     int getAltitude();
+
     int getSpeed();
+
+    /**
+     * Message has valid coordinates
+     */
     boolean isValid();
 }
