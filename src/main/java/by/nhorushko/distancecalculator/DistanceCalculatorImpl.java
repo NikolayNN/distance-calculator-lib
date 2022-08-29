477228c1102347d37c309d25ed261cdd9dff465e
NikolayNN <nikolay.horushko@gmail.com>
2022-08-29 14:50:22 +0300

    refactoring

--- src/main/java/by/nhorushko/distancecalculator/DistanceCalculatorImpl.java
+++ src/main/java/by/nhorushko/distancecalculator/DistanceCalculatorImpl.java
@@ -33,15 +33,9 @@ public class DistanceCalculatorImpl implements DistanceCalculator {
      * @return Distance in Kilometers if calculated distance > {@param maxValidDistanceMeters} return 0
      */
     public double calculateDistance(LatLngAlt pointA, LatLngAlt pointB, DistanceCalculatorSettings settings) {
-        if (
-                isPointsNotValid(pointA, pointB)
-                        || hasWrongOrder(pointA, pointB)
-                        || notHasDetectionSpeed(pointA, pointB, settings)
-                        || isTimeouted(pointA, pointB, settings)
-        ) {
+        if (isPointsNotCalculable(pointA, pointB, settings)) {
             return 0;
         }
-
         double latDistance = degToRad(pointA.getLatitude() - pointB.getLatitude());
         double lngDistance = degToRad(pointA.getLongitude() - pointB.getLongitude());
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
@@ -51,12 +45,31 @@ public class DistanceCalculatorImpl implements DistanceCalculator {
         double distance = AVERAGE_RADIUS_OF_EARTH_METERS * c;
         double height = pointA.getAltitude() - pointB.getAltitude();
         distance = Math.sqrt(distance * distance + height * height);
+        return checkByMaxDistance(distance, settings);
+    }
 
-        return distance > settings.getMaxMessageDistance() ? 0 : distance / 1000D;
+    private double checkByMaxDistance(double distanceMeters, DistanceCalculatorSettings settings) {
+        return distanceMeters > settings.getMaxMessageDistance() ? 0 : distanceMeters / 1000D;
+    }
+
+    private boolean isPointsNotCalculable(LatLngAlt pointA, LatLngAlt pointB, DistanceCalculatorSettings settings) {
+        return isPointsNotValid(pointA, pointB)
+                || hasWrongOrder(pointA, pointB)
+                || notHasDetectionSpeed(pointA, pointB, settings)
+                || isTimeouted(pointA, pointB, settings);
+    }
+
+    private boolean hasWrongOrder(LatLngAlt pointA, LatLngAlt pointB) {
+        return pointB.getDatetime().isBefore(pointA.getDatetime());
     }
 
     private boolean notHasDetectionSpeed(LatLngAlt pointA, LatLngAlt pointB, DistanceCalculatorSettings settings) {
-        return pointA.getSpeed() <= settings.getMinDetectionSpeed() && pointB.getSpeed() <= settings.getMinDetectionSpeed();
+        return pointA.getSpeed() <= settings.getMinDetectionSpeed()
+                && pointB.getSpeed() <= settings.getMinDetectionSpeed();
+    }
+
+    private boolean isTimeouted(LatLngAlt pointA, LatLngAlt pointB, DistanceCalculatorSettings settings) {
+        return pointB.getDatetime().getEpochSecond() - pointA.getDatetime().getEpochSecond() > settings.getMaxMessageTimeout();
     }
 
     private boolean isPointsNotValid(LatLngAlt pointA, LatLngAlt pointB) {
@@ -65,17 +78,9 @@ public class DistanceCalculatorImpl implements DistanceCalculator {
                 || isZeroCoordinates(pointA, pointB);
     }
 
-    private boolean hasWrongOrder(LatLngAlt pointA, LatLngAlt pointB) {
-        return pointB.getDatetime().isBefore(pointA.getDatetime());
-    }
-
     private boolean isZeroCoordinates(LatLngAlt pointA, LatLngAlt pointB) {
         return (pointA.getLatitude() == 0 && pointA.getLongitude() == 0) ||
                 (pointB.getLatitude() == 0 && pointB.getLongitude() == 0);
-    }
-
-    private boolean isTimeouted(LatLngAlt pointA, LatLngAlt pointB, DistanceCalculatorSettings settings) {
-        return pointB.getDatetime().getEpochSecond() - pointA.getDatetime().getEpochSecond() > settings.getMaxMessageTimeout();
     }
 
     private double degToRad(double deg) {
