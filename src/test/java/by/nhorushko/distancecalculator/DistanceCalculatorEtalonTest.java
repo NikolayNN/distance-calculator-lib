59c91784a8e0878ffe94a7a22c31aa3c9f4c9271
Nikolay <nikolay.horushko@gmail.com>
2023-01-30 18:31:26 +0300

    add etalon mileage tests

---
+++ src/test/java/by/nhorushko/distancecalculator/DistanceCalculatorEtalonTest.java
@@ -0,0 +1,71 @@
+package by.nhorushko.distancecalculator;
+
+import by.nhorushko.util.MessageFileReader;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class DistanceCalculatorEtalonTest {
+    private static final String PATH = "src/test/resources/tracks/etalon";
+
+    DistanceCalculator calculator = new DistanceCalculatorImpl();
+
+    @Test
+    public void check_11429() {
+        double etalon = 11.4296;
+        assertMileage(etalon, 11.477, calculate("track-11429-meters-5723.csv"));
+        assertMileage(etalon, 11.465, calculate("track-11429-meters-5724.csv"));
+        assertMileage(etalon, 11.436, calculate("track-11429-meters-5725.csv"));
+    }
+
+    @Test
+    public void check_8280() {
+        double etalon = 8.280698;
+        assertMileage(etalon, 8.364, calculate("track-8280-meters-5723.csv"));
+        assertMileage(etalon, 8.349, calculate("track-8280-meters-5724.csv"));
+        assertMileage(etalon, 8.186, calculate("track-8280-meters-5725.csv"));
+    }
+
+    @Test
+    public void check_72640() {
+        double etalon = 72.640508;
+        assertMileage(etalon, 72.876, calculate("track-72640-meters-5723.csv"));
+        assertMileage(etalon, 72.836, calculate("track-72640-meters-5724.csv"));
+        assertMileage(etalon, 72.868, calculate("track-72640-meters-5725.csv"));
+    }
+
+    @Test
+    public void check_7541() {
+        double etalon = 7.541945;
+        assertMileage(etalon, 7.438, calculate("track-7541-meters-5723.csv"));
+        assertMileage(etalon, 7.476, calculate("track-7541-meters-5724.csv"));
+        assertMileage(etalon, 7.486, calculate("track-7541-meters-5725.csv"));
+    }
+
+    @Test
+    public void check_12018() {
+        double etalon = 12.018727;
+        assertMileage(etalon, 11.982, calculate("track-12018-meters-5723.csv"));
+        assertMileage(etalon, 12.057, calculate("track-12018-meters-5724.csv"));
+        assertMileage(etalon, 12.061, calculate("track-12018-meters-5725.csv"));
+    }
+
+
+
+    private void assertMileage(double etalon, double expected, double actual) {
+        assertWithEpsilon(etalon, actual);
+        assertEquals(expected, actual, 0.001);
+    }
+
+    private void assertWithEpsilon(double expected, double actual) {
+        assertEquals("etalon value big difference", expected, actual, expected * 0.016);
+    }
+
+    private double calculate(String fileName) {
+        List<LatLngAlt> coordinates = MessageFileReader.read(PATH + "/" + fileName);
+        return calculator.calculateDistance(coordinates, DistanceCalculatorSettingsImpl.defaultValue());
+    }
+}
--- src/test/java/by/nhorushko/util/MessageFileReader.java
+++ src/test/java/by/nhorushko/util/MessageFileReader.java
@@ -7,30 +7,52 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MessageFileReader {
 
+    private static final DateTimeFormatter PS_TIMESTAMP_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);
+    private static final ZoneId UTC = ZoneId.of("UTC");
+
     public static List<LatLngAlt> read(String fileName) {
-        List<LatLngAlt> coordinate = new ArrayList<>();
+        List<LatLngAlt> coordinates = new ArrayList<>();
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            coordinate = stream
+            coordinates = stream
                     .map(coordinateString -> {
-                        String[] latLngAltArray = coordinateString.split(";");
-                        return new LatLngAltImpl(
-                                Instant.ofEpochSecond(System.currentTimeMillis() / 1000),
-                                Float.parseFloat(latLngAltArray[0]),
-                                Float.parseFloat(latLngAltArray[1]),
-                                Integer.parseInt(latLngAltArray[2]),
-                                25, true);
+                        String[] source = coordinateString.split(",|;");
+                        if (source.length == 3) {
+                            return new LatLngAltImpl(
+                                    Instant.ofEpochSecond(System.currentTimeMillis() / 1000),
+                                    Float.parseFloat(source[0]),
+                                    Float.parseFloat(source[1]),
+                                    Integer.parseInt(source[2]),
+                                    25, true);
+                        } else {
+                            return new LatLngAltImpl(
+                                    convertTime(source[0]),
+                                    Float.parseFloat(source[1]),
+                                    Float.parseFloat(source[2]),
+                                    Integer.parseInt(source[3]),
+                                    Integer.parseInt(source[4]),
+                                    "VALID".equals(source[5])
+                            );
+                        }
                     })
                     .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return coordinate;
+        return coordinates;
+    }
+
+    private static Instant convertTime(String psTimestamp) {
+        return LocalDateTime.parse(psTimestamp, PS_TIMESTAMP_FORMAT).atZone(UTC).toInstant();
     }
 }
